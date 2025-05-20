170b51e9b2c80616b801ccda1fb6d38209f9641a
tai160903 <ltt16092003@gmail.com>
2025-05-20 19:51:10 +0700

    add facility staff message and sort by alphabet, add CRUD facility staff

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,9 +8,13 @@ router.use("/blood-component", require("./bloodComponent"));
 router.use("/blood-group", require("./bloodGroup"));
 router.use("/blood-compatibility", require("./bloodCompatibility"));
 router.use("/users", require("./user"));
-router.use("/blood-donation-registrations", require("./bloodDonationRegistration"));
+router.use(
+  "/blood-donation-registrations",
+  require("./bloodDonationRegistration")
+);
 router.use("/blood-donations", require("./bloodDonation"));
 router.use("/content-category", require("./contentCategory"));
 router.use("/content", require("./content"));
 router.use("/facility", require("./facility"));
+router.use("/facility-staff", require("./facilityStaff"));
 module.exports = router;
