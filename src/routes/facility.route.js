f47d4c276b72a88b7b77e7bc69558a4537800dd6
tai160903 <ltt16092003@gmail.com>
2025-05-20 10:54:44 +0700

    add check role for facility and add route feedback

--- src/routes/facility.route.js
+++ src/routes/facility.route.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const facilityController = require("../../controllers/facility.controller");
+const { checkRole } = require("../auth/checkAuth");
+const { USER_ROLE } = require("../constants/enum");
 
 const router = express.Router();
 
 router.get("/", facilityController.getAllFacilities);
 router.get("/:id", facilityController.getFacilityById);
 
+router.use(checkRole(USER_ROLE.STAFF, USER_ROLE.ADMIN));
 router.post("/", facilityController.createFacility);
-
 router.put("/:id", facilityController.updateFacility);
 router.put("delete/:id", facilityController.deleteFacility);
 
