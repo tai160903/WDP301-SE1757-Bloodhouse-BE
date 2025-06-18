cbfd4c11c88731238d9e264bcbc55ee24e45dab3
levanhuyhoang <lvhhoangg171@gmail.com>
2025-06-19 00:18:21 +0700

    be: fix create blood unit

--- src/services/bloodDonation.service.js
+++ src/services/bloodDonation.service.js
@@ -64,27 +64,27 @@ class BloodDonationService {
     // }
 
     // Lấy lần hiến máu gần nhất
-    const lastDonation = await bloodDonationModel
-      .findOne({ userId })
-      .sort({ donationDate: -1 });
+    // const lastDonation = await bloodDonationModel
+    //   .findOne({ userId })
+    //   .sort({ donationDate: -1 });
 
-    if (lastDonation) {
-      const lastDonationDate = new Date(lastDonation.donationDate);
-      const currentDate = new Date();
-      const monthsDiff =
-        (currentDate.getFullYear() - lastDonationDate.getFullYear()) * 12 +
-        (currentDate.getMonth() - lastDonationDate.getMonth());
+    // if (lastDonation) {
+    //   const lastDonationDate = new Date(lastDonation.donationDate);
+    //   const currentDate = new Date();
+    //   const monthsDiff =
+    //     (currentDate.getFullYear() - lastDonationDate.getFullYear()) * 12 +
+    //     (currentDate.getMonth() - lastDonationDate.getMonth());
 
-      // Kiểm tra thời gian chờ dựa trên giới tính
-      const requiredMonths = user.gender === "female" ? 4 : 3;
-      if (monthsDiff < requiredMonths) {
-        throw new BadRequestError(
-          `Bạn cần đợi đủ ${requiredMonths} tháng kể từ lần hiến máu trước (${lastDonationDate.toLocaleDateString(
-            "vi-VN"
-          )})`
-        );
-      }
-    }
+    //   // Kiểm tra thời gian chờ dựa trên giới tính
+    //   const requiredMonths = user.gender === "female" ? 4 : 3;
+    //   if (monthsDiff < requiredMonths) {
+    //     throw new BadRequestError(
+    //       `Bạn cần đợi đủ ${requiredMonths} tháng kể từ lần hiến máu trước (${lastDonationDate.toLocaleDateString(
+    //         "vi-VN"
+    //       )})`
+    //     );
+    //   }
+    // }
 
     // Lấy location từ profile người dùng
     const location = user.location || { type: "Point", coordinates: [0, 0] };
